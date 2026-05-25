d05b9913f95b769b7a3737592e3c3865749f6033
212874798 <212874798@qq.com>
2026-05-26 02:01:39 +0800

    feat:实现撮合引擎主题代码

--- orderbook_test.go
+++ orderbook_test.go
@@ -2,8 +2,15 @@ package main
 
 import (
 	"fmt"
+	"reflect"
 	"testing"
 )
+
+func assert(t *testing.T, a, b any) {
+	if !reflect.DeepEqual(a, b) {
+		t.Errorf("Expected %v, got %v", a, b)
+	}
+}
 
 func TestLimit(t *testing.T) {
 	l := NewLimit(100.0)
@@ -21,12 +28,58 @@ func TestLimit(t *testing.T) {
 	fmt.Println(l)
 }
 
-func TestOrderbook(t *testing.T) {
+func TestPlaceLimitOrder(t *testing.T) {
 	ob := NewOrderbook()
 
-	buyOrderA := NewOrder(5, true)
-	ob.PlaceOrder(18, buyOrderA)
+	sellOrderA := NewOrder(5, false)
+	sellOrderB := NewOrder(10, false)
+	ob.PlaceLimitOrder(100.0, sellOrderA)
+	ob.PlaceLimitOrder(100.0, sellOrderB)
 
-	fmt.Println(ob)
+	assert(t, len(ob.asks), 1)
+	assert(t, ob.Asks()[0].Price, 100.0)
+	assert(t, len(ob.Asks()[0].Orders), 2)
+	assert(t, ob.Asks()[0].Orders[0], sellOrderA)
+	assert(t, ob.Asks()[0].Orders[1], sellOrderB)
+}
+func TestPlaceMarketOrder(t *testing.T) {
+	ob := NewOrderbook()
+	sellOrderA := NewOrder(5, false)
+	sellOrderB := NewOrder(10, false)
+	ob.PlaceLimitOrder(100.0, sellOrderA)
+	ob.PlaceLimitOrder(100.0, sellOrderB)
 
+	buyOrder := NewOrder(12, true)
+	matches := ob.PlaceMarketOrder(buyOrder)
+	assert(t, len(matches), 2)
+	assert(t, matches[0].SizeFilled, 5.0)
+	assert(t, matches[0].Price, 100.0)
+	assert(t, matches[1].SizeFilled, 7.0)
+	assert(t, matches[1].Price, 100.0)
+	assert(t, buyOrder.Size, 0.0)
+	assert(t, sellOrderA.Size, 0.0)
+	assert(t, sellOrderB.Size, 3.0)
+
+	//测试流动性不足的情况
+	buyOrder2 := NewOrder(20, true)
+	defer func() {
+		if r := recover(); r == nil {
+			t.Errorf("Expected panic due to insufficient liquidity, but did not panic")
+		}
+
+	}()
+	ob.PlaceMarketOrder(buyOrder2)
+}
+
+func TestCancelOrder(t *testing.T) {
+	ob := NewOrderbook()
+	sellOrderA := NewOrder(5, false)
+	sellOrderB := NewOrder(10, false)
+	ob.PlaceLimitOrder(100.0, sellOrderA)
+	ob.PlaceLimitOrder(100.0, sellOrderB)
+	ob.CancelOrder(sellOrderA)
+	assert(t, len(ob.asks), 1)
+	assert(t, ob.Asks()[0].Price, 100.0)
+	assert(t, len(ob.Asks()[0].Orders), 1)
+	assert(t, ob.Asks()[0].Orders[0], sellOrderB)
 }
